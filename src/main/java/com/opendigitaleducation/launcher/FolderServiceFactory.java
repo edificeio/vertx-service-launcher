c38538a7da8f149404b5747f42b5820c6eb21684
Junior BERNARD <junior.bernard@opendigitaleducation.com>
2025-01-29 17:21:34 +0100

    feat: #ENABLING-213, java 21 migration

--- src/main/java/com/opendigitaleducation/launcher/FolderServiceFactory.java
+++ src/main/java/com/opendigitaleducation/launcher/FolderServiceFactory.java
@@ -7,13 +7,18 @@ import com.opendigitaleducation.launcher.utils.ZipUtils;
 import io.vertx.core.*;
 import io.vertx.core.impl.logging.Logger;
 import io.vertx.core.impl.logging.LoggerFactory;
+import io.vertx.core.json.DecodeException;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.spi.VerticleFactory;
 import io.vertx.service.ServiceVerticleFactory;
 
 import java.io.File;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.Collections;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.util.concurrent.Callable;
 
@@ -96,7 +101,7 @@ public class FolderServiceFactory extends ServiceVerticleFactory {
                                 URLClassLoader urlClassLoader = new URLClassLoader(
                                     new URL[]{dirUrl}, classLoader);
                                 deploymentOptions.setClassLoader(urlClassLoader);
-                                FolderServiceFactory.super.createVerticle(id, deploymentOptions, urlClassLoader, resolution);
+                                doCreateVerticle(id, deploymentOptions, urlClassLoader, resolution);
                             } catch (MalformedURLException e) {
                                 logger.error("Error while trying to deploy " + identifier, e);
                                 resolution.fail(e);
@@ -115,6 +120,79 @@ public class FolderServiceFactory extends ServiceVerticleFactory {
                 resolution.fail(ar.cause());
             }
 		});
+    }
+
+    protected void doCreateVerticle(String verticleName, DeploymentOptions deploymentOptions, ClassLoader classLoader, Promise<Callable<Verticle>> promise) {
+        String identifier = VerticleFactory.removePrefix(verticleName);
+        String descriptorFile = identifier + ".json";
+
+        try (InputStream is = classLoader.getResourceAsStream(descriptorFile)) {
+            if (is == null) {
+                throw new IllegalArgumentException("Cannot find service descriptor file " + descriptorFile + " on classpath");
+            }
+
+            JsonObject descriptor;
+            try {
+                Scanner scanner = (new Scanner(is, "UTF-8")).useDelimiter("\\A");
+                Throwable var12 = null;
+
+                try {
+                    String conf = scanner.next();
+                    descriptor = new JsonObject(conf);
+                } catch (Throwable var41) {
+                    var12 = var41;
+                    throw var41;
+                } finally {
+                    if (scanner != null) {
+                        if (var12 != null) {
+                            try {
+                                scanner.close();
+                            } catch (Throwable var40) {
+                                var12.addSuppressed(var40);
+                            }
+                        } else {
+                            scanner.close();
+                        }
+                    }
+
+                }
+            } catch (NoSuchElementException var43) {
+                throw new IllegalArgumentException(descriptorFile + " is empty");
+            } catch (DecodeException var44) {
+                throw new IllegalArgumentException(descriptorFile + " contains invalid json");
+            }
+
+            String main = descriptor.getString("main");
+            if (main == null) {
+                throw new IllegalArgumentException(descriptorFile + " does not contain a main field");
+            }
+
+            JsonObject serviceOptions = descriptor.getJsonObject("options", new JsonObject());
+            JsonObject mergedDeploymentOptions = deploymentOptions.toJson();
+            mergedDeploymentOptions.mergeIn(serviceOptions);
+            promise.complete((Callable)() -> new AbstractVerticle() {
+                public void start(Promise<Void> startPromise) {
+                    DeploymentOptions dopt = new DeploymentOptions(mergedDeploymentOptions);
+                    if (dopt.getConfig() == null) {
+                        dopt.setConfig(new JsonObject());
+                    }
+
+                    dopt.getConfig().mergeIn(this.context.config());
+                    dopt.setClassLoader(classLoader);
+                    this.vertx.deployVerticle(main, dopt, (ar) -> {
+                        if (ar.succeeded()) {
+                            startPromise.complete();
+                        } else {
+                            startPromise.fail(ar.cause());
+                        }
+
+                    });
+                }
+            });
+        } catch (Exception e) {
+            promise.fail(e);
+        }
+
     }
 
     @Override
